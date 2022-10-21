17ae6d22d13a038da41f8c826445fcdf5f4f336d
David Höld <6930035+dhoeld@users.noreply.github.com>
2022-10-21 08:53:48 +0200

    feat: Add support for conditional creation of kube-prometheus-stack namespace

--- modules/kubernetes-addons/kube-prometheus-stack/main.tf
+++ modules/kubernetes-addons/kube-prometheus-stack/main.tf
@@ -6,6 +6,8 @@ module "helm_addon" {
 }
 
 resource "kubernetes_namespace_v1" "prometheus" {
+  count = try(local.helm_config["create_namespace"], true) && local.helm_config["namespace"] != "kube-system" ? 1 : 0
+
   metadata {
     name = local.helm_config["namespace"]
     labels = {
